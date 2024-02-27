8c338e2ddafa1073489110005df021119219604a
Dika Bošnjak <dika.bosnjak@skyline.be>
2024-02-27 12:44:07 +0100

    Reverted script name changes

---
+++ IDP_Custom_ConfigurationBackup_ImagineCommunicationsSnp_6/BackupFailedException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+[Serializable]
+public class BackupFailedException : Exception
+{
+	public BackupFailedException()
+	{
+	}
+
+	public BackupFailedException(string message) : base(message)
+	{
+	}
+
+	public BackupFailedException(string message, Exception innerException) : base(message, innerException)
+	{
+	}
+
+	protected BackupFailedException(SerializationInfo info, StreamingContext context) : base(info, context)
+	{
+	}
+}
